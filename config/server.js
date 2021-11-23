07dc52c1367a26b88bc6d0d484dd9a954c924382
IHIutch <jbhutch01@gmail.com>
2021-11-23 15:31:18 -0500

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1238a585ed5aacae3391570058d57a9f'),
+    },
+  },
+});
