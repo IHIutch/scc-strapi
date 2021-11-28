d174b52f5dec231ef11b1dc1264caeb53228f77a
IHIutch <jbhutch01@gmail.com>
2021-11-28 14:40:45 -0500

    Handle generating slugs

--- api/events/models/events.js
+++ api/events/models/events.js
@@ -1,8 +1,25 @@
-'use strict';
+"use strict";
+
+const slugify = require("slugify");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    async beforeCreate(data) {
+      if (data.title) {
+        data.slug = slugify(data.title, { lower: true });
+      }
+      return;
+    },
+    async beforeUpdate(params, data) {
+      if (data.title) {
+        data.slug = slugify(data.title, { lower: true });
+      }
+      return;
+    },
+  },
+};
