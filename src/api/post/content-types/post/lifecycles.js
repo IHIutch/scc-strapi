ddbd907fff5f4b123343d5d35a3cc5c563b26bd0
JB Hutch <jbhutch01@gmail.com>
2022-05-16 23:57:18 -0400

    Add ondemand revalidation for published posts

---
+++ src/api/post/content-types/post/lifecycles.js
@@ -0,0 +1,22 @@
+const axios = require("axios");
+
+module.exports = {
+  async afterUpdate({ result }) {
+    if (result.publishedAt) {
+      try {
+        const baseUrl =
+          process.env.NODE_ENV === "production"
+            ? process.env.STRAPI_PREVIEW_PUBLISHED_URL
+            : "http://localhost:3000";
+
+        await axios.get(`${baseUrl}/api/revalidate/blog/${result.slug}`, {
+          params: {
+            secret: process.env.STRAPI_PREVIEW_SECRET,
+          },
+        });
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    }
+  },
+};
