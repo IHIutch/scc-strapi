6fcb07d67b96d9b5be299eecb0c03e127e131e27
JB Hutch <jbhutch01@gmail.com>
2022-12-05 12:11:25 -0500

    Update lifecycle events

--- src/api/event/content-types/event/lifecycles.js
+++ src/api/event/content-types/event/lifecycles.js
@@ -18,22 +18,4 @@ module.exports = {
       });
     }
   },
-  async afterUpdate({ result }) {
-    if (result.publishedAt) {
-      try {
-        const baseUrl =
-          process.env.NODE_ENV === "production"
-            ? process.env.STRAPI_PREVIEW_PUBLISHED_URL
-            : "http://localhost:3000";
-
-        await axios.get(`${baseUrl}/api/revalidate/events/${result.slug}`, {
-          params: {
-            secret: process.env.STRAPI_PREVIEW_SECRET,
-          },
-        });
-      } catch (error) {
-        console.error(`Revalidation error: ${error.message}`);
-      }
-    }
-  },
 };
