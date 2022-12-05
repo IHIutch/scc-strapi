587a68b2a64bdc9bf5901b7d43340c2df20e6a57
JB Hutch <jbhutch01@gmail.com>
2022-12-05 12:25:42 -0500

    Update preview urls

--- config/plugins.js
+++ config/plugins.js
@@ -1,63 +1,68 @@
-module.exports = ({ env }) => ({
-  "users-permissions": {
-    config: {
-      jwtSecret: env("JWT_SECRET"),
-    },
-  },
-  sitemap: {
-    enabled: true,
-    config: {
-      autoGenerate: true,
-      allowedFields: ["id", "string"],
-    },
-  },
-  "preview-button": {
-    enabled: true,
-    config: {
-      contentTypes: [
-        {
-          uid: "api::event.event",
-          targetField: "slug",
-          published: {
-            url: process.env.STRAPI_PREVIEW_PUBLISHED_URL + "/events/{slug}",
-          },
-          draft: {
-            url: process.env.STRAPI_PREVIEW_DRAFT_URL,
-            query: {
-              type: "event",
-              slug: "{slug}",
-            },
-          },
-        },
-        {
-          uid: "api::post.post",
-          targetField: "slug",
-          published: {
-            url: process.env.STRAPI_PREVIEW_PUBLISHED_URL + "/blog/{slug}",
-          },
-          draft: {
-            url: process.env.STRAPI_PREVIEW_DRAFT_URL,
-            query: {
-              type: "post",
-              slug: "{slug}",
-            },
-          },
-        },
-      ],
-    },
-  },
-  upload: {
-    config: {
-      provider: "cloudinary",
-      providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
-      },
-      actionOptions: {
-        upload: {},
-        delete: {},
+module.exports = ({ env }) => {
+  const previewUrl =
+    (process.env.NODE_ENV === "production"
+      ? process.env.STRAPI_SITE_URL
+      : "http://localhost:3000") + "/api/preview";
+
+  return {
+    "users-permissions": {
+      config: {
+        jwtSecret: env("JWT_SECRET"),
       },
     },
-  },
-});
+    sitemap: {
+      enabled: true,
+      config: {
+        autoGenerate: true,
+        allowedFields: ["id", "string"],
+      },
+    },
+    "preview-button": {
+      enabled: true,
+      config: {
+        contentTypes: [
+          {
+            uid: "api::event.event",
+            published: {
+              url: process.env.STRAPI_SITE_URL + "/events/{slug}",
+            },
+            draft: {
+              url: previewUrl,
+              query: {
+                type: "event",
+                slug: "{slug}",
+              },
+            },
+          },
+          {
+            uid: "api::post.post",
+            published: {
+              url: process.env.STRAPI_SITE_URL + "/blog/{slug}",
+            },
+            draft: {
+              url: previewUrl,
+              query: {
+                type: "post",
+                slug: "{slug}",
+              },
+            },
+          },
+        ],
+      },
+    },
+    upload: {
+      config: {
+        provider: "cloudinary",
+        providerOptions: {
+          cloud_name: env("CLOUDINARY_NAME"),
+          api_key: env("CLOUDINARY_KEY"),
+          api_secret: env("CLOUDINARY_SECRET"),
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
+  };
+};
