1cbbdf3a2e1bb920880a24e2eb600e8c915c9f60
JB Hutch <jbhutch01@gmail.com>
2024-01-25 20:55:24 -0500

    Update node version

---
+++ types/generated/components.d.ts
@@ -0,0 +1,79 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface SharedLocation extends Schema.Component {
+  collectionName: 'components_shared_locations';
+  info: {
+    displayName: 'location';
+    icon: 'map-pin';
+    description: '';
+  };
+  attributes: {
+    streetAddress: Attribute.String;
+    city: Attribute.String;
+    state: Attribute.String;
+    googleMapsUrl: Attribute.String;
+    zipCode: Attribute.String;
+    title: Attribute.String;
+  };
+}
+
+export interface SharedMetaSocial extends Schema.Component {
+  collectionName: 'components_shared_meta_socials';
+  info: {
+    displayName: 'metaSocial';
+    icon: 'project-diagram';
+  };
+  attributes: {
+    socialNetwork: Attribute.Enumeration<['Facebook', 'Twitter']> &
+      Attribute.Required;
+    title: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        maxLength: 60;
+      }>;
+    description: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        maxLength: 65;
+      }>;
+    image: Attribute.Media;
+  };
+}
+
+export interface SharedSeo extends Schema.Component {
+  collectionName: 'components_shared_seos';
+  info: {
+    displayName: 'seo';
+    icon: 'search';
+  };
+  attributes: {
+    metaTitle: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        maxLength: 60;
+      }>;
+    metaDescription: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        minLength: 50;
+        maxLength: 160;
+      }>;
+    metaImage: Attribute.Media & Attribute.Required;
+    metaSocial: Attribute.Component<'shared.meta-social', true>;
+    keywords: Attribute.Text;
+    metaRobots: Attribute.String;
+    structuredData: Attribute.JSON;
+    metaViewport: Attribute.String;
+    canonicalURL: Attribute.String;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'shared.location': SharedLocation;
+      'shared.meta-social': SharedMetaSocial;
+      'shared.seo': SharedSeo;
+    }
+  }
+}
